1932cf4a570214fb77cf593d3445cf7237784e58
Iryna Bernyk <BernykI@cardiff.ac.uk>
2018-03-11 21:36:08 +0000

    Some methods are added

--- QuizTeam10/src/quizteam10/QuizTeam10.java
+++ QuizTeam10/src/quizteam10/QuizTeam10.java
@@ -5,5 +5,47 @@ public class QuizTeam10 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    private String[] tempQuestions;
+    
+    public static void selectQuestion(String[] question) {
+        // Code here
+    }
+    
+    public static void storeQuestionInTemp(){
+        // Code here
+    }
+    
+    public static void dispalayQuestionAndPossibleAnswers(){
+        // Code here
+    }
+    
+    public static void getAnswer(){
+        // Code here
+    }
+    
+    public static void validateAnswer() {
+        // Code here
+    }
+    
+    public static void provideFeedback(){
+        // Code here
+    }
+    
+    public static void provideOptionsToStudent() {
+        // Code here 
+    }
+    
+    public static void displayQuestionToAdmin() {
+        // Code here
+    }
+    
+    public static void openWindowAdd() {
+        // Code here
+    }
+    
+    public static void openWindowEdit() {
+        // Code here
+    }
+    
     
 }
